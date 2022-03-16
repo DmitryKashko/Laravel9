281dd35f9c3d9b7b917da2cdc3c8cd385d9cce53
DmitryKashko <kashko.dmitry@gmail.com>
2022-03-16 15:27:19 +0300

    adding a ProjectController

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ProjectRequest;
+use App\Http\Resources\ProjectResource;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
+    public function index()
+    {
+        /*return Project::all();*/
+        return ProjectResource::collection(Project::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return ProjectResource
+     */
+    public function store(ProjectRequest $request)
+    {
+        $created_project = Project::create($request->validated());
+
+        return new ProjectResource($created_project);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return ProjectResource|\Illuminate\Http\Response
+     */
+    public function show(Project $project)
+    {
+        return new ProjectResource($project);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return ProjectResource
+     */
+    public function update(ProjectRequest $request, Project $project)
+    {
+        $project->update($request->validated());
+
+        return new ProjectResource($project);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Project $project)
+    {
+        $project->delete();
+
+        return response()->noContent();
+    }
+}
